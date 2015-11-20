b5edf67483a45ae8a73f2f7fc66f2e286ced5221
shilpathakur95 <thakur.shilpa95@gmail.com>
2015-11-20 17:28:23 +0530

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -17,5 +17,63 @@ NOTES: If there are no common words return NULL.
 #define SIZE 31
 
 char ** commonWords(char *str1, char *str2) {
-	return NULL;
+	char **string;
+	string = (char **)malloc(SIZE*sizeof(char *));
+	int iterator = 0, iterator2 = 0, iterator3 = 0, iterator4 = 0, iterator5 = -1, it = 0, len = 0, len2 = 0;
+	if (str1 == NULL || str2 == NULL)
+		return NULL;
+	for (iterator = 0; iterator < SIZE; iterator++)
+	{
+		string[iterator] = (char *)malloc(SIZE*sizeof(char));
+	}
+	iterator = 0;
+	while (str1[iterator] != '\0')
+	{
+		iterator2 = iterator;
+		iterator3 = 0;
+		it = iterator;
+		len2 = 0;
+		while (str1[it] != ' ')
+		{
+			len2++;
+			it++;
+		}
+		if (str1[iterator] != ' ')
+		{
+			while ((str2[iterator3] != str1[iterator2]) && str2[iterator3] != '\0')
+			{
+				iterator3++;
+			}
+			if (str2[iterator3] == str1[iterator] && (str2[iterator3 - 1] == ' ' || iterator3 == 0))
+			{
+				len = 0;
+				while (str1[iterator2] != ' ')
+				{
+					if (str2[iterator3] == str1[iterator2])
+					{
+						iterator2++;
+						iterator3++;
+						len++;
+					}
+					else
+						break;
+				}
+			}
+		}
+		if (len == len2 && iterator2 != iterator)
+		{
+			iterator5 = 0;
+			for (iterator3 = iterator; iterator3 < iterator2; iterator3++)
+			{
+				string[iterator4][iterator5] = str1[iterator3];
+				iterator5++;
+			}
+			string[iterator4][iterator5] = '\0';
+			iterator4++;
+		}
+		iterator++;
+	}
+	if (iterator5 < 0)
+		return NULL;
+	return string;
 }
